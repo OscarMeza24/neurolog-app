527237bd6a2875cec0ed46b0d64ad83e01841f42
OscarMeza24 <omeza2411@gmai.com>
2025-06-13 14:19:32 -0500

    Primer commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 // src/app/page.tsx
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
